ee2c70911d61ed021227f40b5e6f7e7cee1b1d1a
jeremy.sarkissian <jeremy.sarkissian@CP-SIO-W10-PG09.bts.sio>
2022-09-13 04:34:11 +0200

    commencement du code en js

--- JS/remuneration.js
+++ JS/remuneration.js
@@ -3,7 +3,29 @@ const xspirit = 350;
 const multitec = 180;
 const s20 = 140;
 
-let nbxpirit = 0;
-let nbmultitec = 0;
-let nbs20 = 0;
 
+
+let nbancien = ParseInt(window.document.querySelector("#ancien").value);
+ if (nbancien >= 5) {
+     fixe = fixe * 1.03;
+ }
+ else if (nbancien >10){
+     fixe = fixe * 1.06;
+ }
+ 
+ let nbsVingt = ParseInt(window.document.querySelector("#nbs20").value);
+ let comS20 = nbsVingt * s20 *0.02; 
+ fixe += comS20; 
+ 
+ let nbxspirit = ParseInt(window.document.querySelector("#nbx-spirit").value);
+ if(nbxspirit >=50){
+     let comxspirit = nbxspirit * xspirit * 0.06;
+     fixe += comxspirit;
+ }
+ 
+ let nbmultitec = ParseInt(window.document.querySelector("#nbmultitec").value);
+ if(nbmultitec <=20){
+     let commultitec = nbmultitec * multitec * 0.04;
+     fixe += commultitec;
+ }
+ else if 
